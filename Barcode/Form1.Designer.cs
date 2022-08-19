fcc78833d40659d376188cb9960f6563bc9bbf63
Mehdi <notset@gmail.com>
2022-08-19 14:15:16 +0900

    first

---
+++ Barcode/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Barcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.maingrid = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maingrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBox1.Location = new System.Drawing.Point(39, 88);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 163);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "　検索　";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "商品名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "仕入先名";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(482, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "商品バーコード印刷";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox1.Location = new System.Drawing.Point(87, 288);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(161, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "商品価格を表示する";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(1040, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "印刷";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::Barcode.Properties.Resources.arrow_left;
+            this.button3.Location = new System.Drawing.Point(605, 520);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 45);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::Barcode.Properties.Resources.arrow_right;
+            this.button2.Location = new System.Drawing.Point(605, 452);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 45);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // maingrid
+            // 
+            this.maingrid.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.maingrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.maingrid.Location = new System.Drawing.Point(39, 328);
+            this.maingrid.Name = "maingrid";
+            this.maingrid.RowTemplate.Height = 21;
+            this.maingrid.Size = new System.Drawing.Size(546, 348);
+            this.maingrid.TabIndex = 7;
+            this.maingrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(710, 153);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 21;
+            this.dataGridView3.Size = new System.Drawing.Size(424, 523);
+            this.dataGridView3.TabIndex = 9;
+            this.dataGridView3.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(1140, 153);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.RowTemplate.Height = 21;
+            this.dataGridView5.Size = new System.Drawing.Size(48, 523);
+            this.dataGridView5.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1230, 711);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.maingrid);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "商品バーコード印刷";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maingrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView maingrid;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView5;
+    }
+}
+
---
+++ Barcode/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Collections;
+
+namespace Barcode
+{
+    public partial class Form1 : Form
+    {
+
+        ConnectionDB con = new ConnectionDB();
+
+        //arraylist to getter and setter data  
+        private static ArrayList Product_Code = new ArrayList();
+        private static ArrayList Product_Name = new ArrayList();
+        private static ArrayList P_UnitPrice = new ArrayList();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetData()
+        {
+            try
+            {
+                con.Open();
+                string query = "select * from Product_New";
+
+                //MySqlDataReader row;  
+                MySqlDataReader row;
+                row = con.ExecuteReader(query);
+                if (row.HasRows)
+                {
+                    while (row.Read())
+                    {
+                        Product_Code.Add(row["Product_Code"].ToString());
+                        Product_Name.Add(row["Product_Name"].ToString());
+                        P_UnitPrice.Add(row["P_UnitPrice"].ToString());
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Data not found");
+                }
+
+                con.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+
+        private void updateDatagrid()
+        {
+            maingrid.Rows.Clear();
+            
+            for (int i = 0; i < Product_Code.Count; i++)
+            {
+                //data
+                DataGridViewRow newRow = new DataGridViewRow();
+                maingrid.ColumnCount = 2;
+                newRow.CreateCells(maingrid);
+                newRow.Cells[0].Value = Product_Code[i];
+                newRow.Cells[1].Value = Product_Name[i];
+                maingrid.Rows.Add(newRow);
+
+                //styles
+                maingrid.Columns[1].Width = 386;
+                maingrid.Columns[0].HeaderText = "商品コード";
+                maingrid.Columns[1].HeaderText = "商品名";
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetData();
+            if (Product_Code.Count > 0)
+            {
+                updateDatagrid();
+            }
+            else
+            {
+                MessageBox.Show("Data not found");
+            }
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (checkBox1.Checked)
+            {
+                maingrid.Rows.Clear();
+
+                for (int i = 0; i < Product_Code.Count; i++)
+                {
+                    //data
+                    DataGridViewRow newRow = new DataGridViewRow();
+                    maingrid.ColumnCount = 3;
+                    newRow.CreateCells(maingrid);
+                    newRow.Cells[0].Value = Product_Code[i];
+                    newRow.Cells[1].Value = Product_Name[i];
+                    newRow.Cells[2].Value = P_UnitPrice[i];
+                    maingrid.Rows.Add(newRow);
+
+                    //styles
+                    maingrid.Columns[1].Width = 326;
+                    maingrid.Columns[2].Width = 60;
+                    maingrid.Columns[0].HeaderText = "商品コード";
+                    maingrid.Columns[1].HeaderText = "商品名";
+                    maingrid.Columns[2].HeaderText = "価格";
+                }
+
+            } else
+            {
+                maingrid.Rows.Clear();
+
+                for (int i = 0; i < Product_Code.Count; i++)
+                {
+                    //data
+                    DataGridViewRow newRow = new DataGridViewRow();
+                    maingrid.ColumnCount = 2;
+                    newRow.CreateCells(maingrid);
+                    newRow.Cells[0].Value = Product_Code[i];
+                    newRow.Cells[1].Value = Product_Name[i];
+
+
+
+                    maingrid.Rows.Add(newRow);
+
+                    //styles
+                    maingrid.Columns[1].Width = 386;
+                    maingrid.Columns[0].HeaderText = "商品コード";
+                    maingrid.Columns[1].HeaderText = "商品名";
+
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string textboxvalue = textBox2.Text;
+
+                maingrid.Rows.Clear();
+                try
+                {
+                    con.Open();
+                    string query = "select * from Product_New where Product_Name like \'%" + textboxvalue + "%\'";
+
+                    //MySqlDataReader row;  
+                    MySqlDataReader row;
+                    row = con.ExecuteReader(query);
+                    if (row.HasRows)
+                    {
+                        while (row.Read())
+                        {
+                            Product_Code.Add(row["Product_Code"].ToString());
+                            Product_Name.Add(row["Product_Name"].ToString());
+                            P_UnitPrice.Add(row["P_UnitPrice"].ToString());
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data not found");
+                    }
+
+                    con.Close();
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.ToString());
+                }
+            }
+        }
+    }
